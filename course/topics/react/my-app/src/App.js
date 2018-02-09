73a6435851d6fa72240a6267d3b306a78d0faf18
Robert Vera <robertveraroige@gmail.com>
2018-02-09 14:42:59 +0100

    :pencil: create react-app test

---
+++ course/topics/react/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import TaskApp from './Components/TaskAPP'
+
+class App extends Component {
+  render() {
+    return (
+      <TaskApp/>
+    );
+  }
+}
+
+export default App;
+
--- course/topics/react/task-app-bootstrap/scripts/main.jsx
+++ course/topics/react/task-app-bootstrap/scripts/main.jsx
@@ -1,69 +1,101 @@
 'use strict';
 
+class Task {
+    constructor(title) {
+        this.id = new Date().getTime()
+        //this.id = Task.count++
+        this.title = title
+        this.done = false
+    }
+
+    //static count = 0
+}
+
 class TaskApp extends React.Component {
     constructor() {
         super()
 
         this.state = {
-            // id = getTime(),
             tasks: []
         }
     }
 
-    addTask = task => {
-        this.setState(prevState => {
-            return {
-                tasks: [...prevState.tasks, { title: task, done: false }]
+    addTask (title) {
+        this.setState(prevState => ({
+                tasks: [...prevState.tasks, new Task(title)]
             }
-        })
+        ))
     }
 
-    checkTask = 
+    checkTask(id) {
+        this.setState(prevState => ({
+            tasks: prevState.tasks.map(task => {
+                if (task.id === id)
+                task.done = true
 
-    removeTask = index => {
-        this.setState(prevState => {
-            tasks: prevState.tasks.map((task, _index) => {
-                if (index === _index) 
-                    task.done = true
+                return task
+            })
+        }))
+    }
 
-                    return task
-                })
-        })
+    checkAllTasks() {
+        this.setState(prevState => ({
+            tasks: prevState.tasks.map(task => {
+                task.done = true
+
+                return task
+            })
+        }))
+
+    }
+
+    removeTask (id) {
+        this.setState(prevState => ({
+            tasks: prevState.tasks.filter(task => task.id !== id)
+        }))
     }
 
     render() {
         return <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <div className="todolist not-done">
-                        <h1>Todos</h1>
-                        <TaskInput onAddTask={this.addTask} />
-                        <button id="checkAll" className="btn btn-success">Mark all as done</button>
-                        <hr />
-                        <TaskList tasks={this.state.tasks} onRemoveTask={this.removeTask} />
-                        <div className="todo-footer">
-                            <strong><span className="count-todos" /></strong> Items Left
-        </div>
-                    </div>
+                    <TodoList onAddTask={this.addTask.bind(this)} onCheckAllTasks={this.checkAllTasks.bind(this)} tasks={this.state.tasks} onCheckTask={this.checkTask.bind(this)} />
                 </div>
                 <div className="col-md-6">
-                    <div className="todolist">
-                        <h1>Already Done</h1>
-                        <ul id="done-items" className="list-unstyled">
-                            <li>Some item <button className="remove-item btn btn-default btn-xs pull-right"><span className="glyphicon glyphicon-remove" /></button></li>
-                        </ul>
-                    </div>
+                    <DoneList tasks={this.state.tasks} onRemoveTask={this.removeTask.bind(this)} />
                 </div>
             </div>
         </div>
-
     }
 
 
 
 }
 
-class TaskInput extends React.Component {
+function TodoList(props) {
+    return <div className="todolist not-done">
+        <h1>Todos</h1>
+        <InputSubmit onSubmit={props.onAddTask.bind(this)} placeholder={'Add todo'} />
+        <SuccessButton onClick={props.onCheckAllTasks} label={'Mark all as done'} />
+        <hr />
+        <ul id="sortable" className="list-unstyled">
+            {
+                props.tasks.map((task) =>
+                    task.done ? '' : <li className="ui-state-default" key={task.id}>
+                        <div className="checkbox">
+                            <label>
+                                <input type="checkbox" defaultValue onClick={e => { e.preventDefault(); props.onCheckTask(task.id) }} />{task.title}</label>
+                        </div>
+                    </li>)
+            }
+        </ul>
+        <div className="todo-footer">
+            <strong><span className="count-todos">{props.tasks.reduce((accum, task) => task.done ? accum : ++accum, 0)}</span></strong> Items Left
+        </div>
+    </div>
+}
+
+class InputSubmit extends React.Component {
     constructor() {
         super()
 
@@ -72,41 +104,47 @@ class TaskInput extends React.Component {
         }
     }
 
-    keepInput = e => this.setState({ input: e.target.value })
+    keepInput(input) {
+        if (input)
+            this.setState({ input })
+    }
 
-    addTask = () => {
-        this.props.onAddTask(this.state.input)
+    submit() {
+        this.props.onSubmit(this.state.input)
 
         this.setState({ input: '' })
     }
 
     render() {
-        return <form onSubmit={e => {
-            e.preventDefault()
-
-            this.addTask()
-        }
-        }>
-            <input type="text" className="form-control add-todo" placeholder="Add todo" onChange={this.keepInput} value={this.state.input} />
+        return <form onSubmit={(e) => { e.preventDefault(); this.submit() }}>
+            <input type="text" className="form-control add-todo" placeholder={this.props.placeholder} onChange={(e) => this.keepInput(e.target.value)} value={this.state.input} required />
         </form>
     }
 
 }
 
-function TaskList(props) {
-    return <ul id="sortable" className="list-unstyled">
-        {props.tasks.map((task, index) => 
-        <li key={index} className="ui-state-default">
-            <div className="checkbox">
-                <label>
-                    <input type="checkbox" defaultValue onChange={e => {
-                        e.preventDefault()
+function SuccessButton(props) {
+    return <button className="btn btn-success checkAll" onClick={e => { e.preventDefault(); props.onClick() }}>{props.label}</button>
+}
 
-                        props.onRemoveTask(index)
-                    }} />{task.title}</label>
-            </div>
-        </li>)}
-    </ul>
+function DoneList(props) {
+    return <div className="todolist">
+        <h1>Already done</h1>
+        <ul id="done-items" className="list-unstyled">
+            {
+                props.tasks.map(task =>
+                    task.done ? <li key={task.id}>{task.title}
+                        <RemoveButton onClick={() => { props.onRemoveTask(task.id) }} />
+                    </li> : '')
+            }
+        </ul>
+    </div>
+}
+
+function RemoveButton(props) {
+    return <button className="remove-item btn btn-default btn-xs pull-right" onClick={e => { e.preventDefault(); props.onClick() }}>
+        <span className="glyphicon glyphicon-remove" />
+    </button>
 }
 
 
