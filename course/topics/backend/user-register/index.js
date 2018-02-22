ab6112893460d8228838869a38c3d9ca5b183666
Robert Vera <robertveraroige@gmail.com>
2018-02-23 00:34:42 +0100

    Express.js - user-register: PUT&DELETE

--- course/topics/backend/user-register/index.js
+++ course/topics/backend/user-register/index.js
@@ -1,44 +1,116 @@
+// require('dotenv').config()
+
 const express = require('express')
+
 const bodyParser = require('body-parser')
+const _ = require('lodash')
 
 const app = express()
-let usersArray = []
 
-app.use(bodyParser({urlencoded: false}))
+const users = []
 
-app.post(('/api/users'), (req,res) => {
-    
-    const user = {
-        username: req.body.username,
-        password: req.body.password
-    }
+app.get('/api/users', (req, res) => res.json(ok('Users listing succeeded.', users.map(({ username }) => ({ username })))))
 
-    res.json(user)
-    usersArray = [...usersArray, user]
+const jsonBodyParser = bodyParser.json()
 
-    if (usersArray.includes(user.username)) {
-        const reply = {
-            status: 'KO',
-            message: 'Cannot register user',
-            error: 'user already exists'
-        }
-    } else {
-        const reply = {
-            status: 'OK',
-            message: 'User registered successfully'            
-        }
-    }
-    
-    res.json(reply)
+app.post('/api/users', jsonBodyParser, (req, res) => {
+    const { username, password } = req.body
 
-    console.log(usersArray)    
-    console.log(reply)
+    if (!username || !password) return res.json(ko('User registration failed.', 'Invalid username and/or password.'))
+
+    const alreadyExists = users.some(user => user.username === username)
+
+    if (alreadyExists) return res.json(ko('User registration failed.', 'Username already exists.'))
+
+    users.push({ username, password })
+
+    res.json(ok('User registration succeeded.'))
 })
 
+app.put('/api/users',jsonBodyParser, (req,res) => {
+    const {username, oldPassword, newPassword} = req.body
 
+    const exists = users.some(user => user.username === username)
 
+    if (!exists) return res.json('User not found')
+    
+    users.map(user=> {
+        if (username === user.username && oldPassword !== user.password) {
+            return res.json(changePasswordKO('Sorry, password NOT UPDATED, old password does not match with the current password.'))
+        }
+    })
 
+    users.map(user=> {
+        if (username === user.username && oldPassword === user.password) {
+            user.password = newPassword
+            return res.json(changePasswordOK('Password updated'))
+        }
+    })    
+})
+
+app.delete('/api/users', jsonBodyParser, (req,res) => {
+    const {username, password} = req.body
+
+    const exists = users.some(user => user.username === username)
+
+    if (!exists) return res.json('User not found')
+
+    users.map(user=> {
+        if (username === user.username && password === user.password) {            
+            _.pull(users, user)
+            return res.json(removeOK('User removed.'))
+        }
+       
+    }) 
+
+    users.map(user=> {
+        if (username === user.username && password !== user.password) {
+            return res.json(removeKO('Wrong password, user not deleted.'))
+        }
+    })
+
+})
+
+function changePasswordOK(message) {
+    const res = {status: 'OK', message}
+
+    return res
+}
+
+function changePasswordKO(message) {
+    const res = {status: 'KO', message}
+
+    return res
+}
+
+function removeOK(message) {
+    const res = {status: 'OK', message}
+
+    return res
+}
+
+function removeKO(message) {
+    const res = {status: 'KO', message}
+
+    return res
+}
+
+function ok(message, data) {
+    const res = { status: 'OK', message }
+
+    if (data) res.data = data
+
+    return res
+}
+
+function ko(message, error) {
+    const res = { status: 'KO', message }
+
+    if (error) res.error = error
+
+    return res
+}
 
 const port = process.argv[2]
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Users API running on port ${port}`))
