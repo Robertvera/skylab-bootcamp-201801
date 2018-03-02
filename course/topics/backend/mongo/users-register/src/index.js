2ac9f93d7c4b23c052a35caca40bb54018acf1d8
Robert Vera <robertveraroige@gmail.com>
2018-03-02 17:31:24 +0100

    :sparkles:users app with mongo and pug

--- course/topics/backend/mongo/users-register/src/index.js
+++ course/topics/backend/mongo/users-register/src/index.js
@@ -3,42 +3,91 @@ require('dotenv').config()
 const express = require('express')
 const { MongoClient } = require('mongodb')
 const bodyParser = require('body-parser')
+const ObjectID = require('mongodb').ObjectID
 
 const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-
 const formBodyParser = bodyParser.urlencoded({ extended: false })
-
-function getUsers() {
-    return 
-}
 
 MongoClient.connect('mongodb://localhost:27017/', (err, conn) => {
     if (err) throw err
 
     const db = conn.db('bootcamp')
 
+    app.get('/', (req, res) => {
+        db.collection('users').find().toArray()
+            .then((users => {
+                res.render('index', { users })
+            }))
+
+    })
+
     app.post('/submit', formBodyParser, (req, res) => {
-        const user = {
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password
-        }
-
-        db.collection('users').insert(user).then(data => res.redirect('/'))
+        const { name, surname, email, username, password } = req.body
+        
+        if (name.length === 0 || surname.length ===0 || email.length === 0 || username.length === 0 ) { 
+            const message = '🚫 Form fields are mandatory'
+            db.collection('users').find().toArray()
+            .then((users => {
+                res.render('index', { users, message })
+            }))            
+        } else if (password.length < 6) {
+            const message = '🚫 Password must be longer than 6 characters'
+            db.collection('users').find().toArray()
+            .then((users => {
+                res.render('index', { users, message })
+            }))            
+        } else if (email === db.collection('users').find({ }, email) ) {
+            const message = '🚫 This email is already in use'
+            db.collection('users').find().toArray()
+            .then((users => {
+                res.render('index', { users, message })
+            }))
+        } else {
+            db.collection('users').insert({ name, surname, email, username, password }).then(data => res.redirect('/'))
+        }        
     })
 
-    app.get('/userslist', (req,res) => {
-        db.collection('users').find().then(res.redirect('/'))
+    app.get('/user/:id', (req, res) => {
+        const { params: { id } } = req
+
+        db.collection('users').find().toArray()
+            .then((data => {
+                res.render('index', { 'users': data, id })
+            }))
+
     })
+
+    app.post('/edit/user/:id', formBodyParser, (req, res) => {
+        const { params: { id } } = req
+        const { name, surname, email, username, password } = req.body
+
+        db.collection('users').update(
+            { '_id': ObjectID(id) },
+            {
+                name: name,
+                surname: surname,
+                email: email,
+                username: username,
+                password: password
+            }
+
+        ).then(res.redirect('/'))
+
+    })
+
+    app.get('/delete/user/:id', (req, res) => {
+        const { params: { id } } = req
+
+        db.collection('users').deleteOne({ '_id': ObjectID(id) }).then(res.redirect('/'))
+    })
+
+    app.get('/killemall', (req, res) => {
+        db.collection('users').remove({}).then(res.redirect('/'))
+    })
+
 
 
     const port = process.env.PORT
@@ -52,6 +101,7 @@ MongoClient.connect('mongodb://localhost:27017/', (err, conn) => {
         process.exit()
     })
 
-
 })
+
+
 
